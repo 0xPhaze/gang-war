ad93135ea506d2cb0c111ca8b71e3a6fa0d57d11
Lumy <lumierius@gmail.com>
2022-10-23 14:39:46 +0200

    push new barons list

--- src/SetupBase.sol
+++ src/SetupBase.sol
@@ -56,7 +56,7 @@ contract SetupBase is UpgradeScripts {
     uint256 constant CHAINID_MUMBAI = 80_001;
     uint256 constant CHAINID_TEST = 31_337;
 
-    uint256 lastDeployConfirmation = 1666456043;
+    uint256 lastDeployConfirmation = 1666527177;
 
     // ROOT
     GMCRoot gmcRoot;
